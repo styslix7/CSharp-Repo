94169286acd32519164104a2e27d3584ebe2a9ae
styslix7 <styslix7@gmail.com>
2024-11-05 22:32:22 +0300

    Re-Added Program files via upload

---
+++ ProgramConsole1.cs
@@ -0,0 +1,13 @@
+﻿namespace Console1.HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            message = "Hello world!";
+            Console.WriteLine(message);
+            Console.WriteLine("Styslix.");
+        }
+    }
+}
