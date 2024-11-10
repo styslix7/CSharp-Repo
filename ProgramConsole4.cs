cd65075e3bf54182f36d202a8048c5905d04585f
Styslix <styslix7@gmail.com>
2024-11-10 16:28:50 +0300

    Add files via upload

---
+++ ProgramConsole4.cs
@@ -0,0 +1,23 @@
+﻿namespace Console4.CharactersAndHexadecimals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //char stands for character which you need to put '' instead of "" to declare since it only takes 1 single space and is not a string.
+            char myFirstInitial = 'B';
+            char myLastInitial = 'P';
+
+            //"b" below stands for backspace which moves the cursor one position backwards and "t" is for tab which moves the cursor to next tab.
+            char backSpace = '\b';
+            char tab = '\t';
+
+            //u0062 is the hexadecimal value for "b", which prints "b"
+            char hexLetter = '\u0062';
+
+            //printings all of them to console with using \" which stands for " itself.
+            Console.WriteLine("\"My first initial= " + myFirstInitial + "\t" + "Last initial= " + myLastInitial + "\"");
+            Console.WriteLine("My first initial in hexadecimal value= " + hexLetter);
+        }
+    }
+}
